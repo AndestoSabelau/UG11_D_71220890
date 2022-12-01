51c8bf14051f0d65535b0d702e33ea8dad966386
AndestoSabelau <117900934+AndestoSabelau@users.noreply.github.com>
2022-12-01 16:15:36 +0700

    Add files via upload

---
+++ 1_UG11_71220890.py
@@ -0,0 +1,31 @@
+#test case 2
+print("Halo selamat datang di bisokop!")
+print("Dimanakah kamu ingin menonton?")
+print("1)XXI Empire")
+print("2)XXI Amplaz")
+print("3)XXI JCM")
+int(input("masukan pilihanmu :"))
+def film():
+   if(4):
+        print("pilihan tidak tersedia")
+        
+
+
+
+film()
+
+#test case 3
+print("Halo selamat datang di bisokop!")
+print("Dimanakah kamu ingin menonton?")
+print("1)XXI Empire")
+print("2)XXI Amplaz")
+print("3)XXI JCM")
+int(input("masukan pilihanmu :"))
+def bioskop():
+   if(1):
+        print("1.frozen ")
+        print("2.keramat ")
+        print("3.KKN di desa penari ")
+        int(input("pilih no film:"))
+        print("pilihan tida tersedia")
+bioskop()
